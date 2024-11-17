adafe9f842944d1c90c3ddc832d0b7487f2fa14f
AlexSemin-cjder <119964665+AlexSemin-cjder@users.noreply.github.com>
2024-11-17 16:31:15 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+grade_book = {}
+
+grades = [[3, 3, 3, 5, 4 ], [2, 2, 2, 3 ], [4, 5, 5, 2 ], [4, 4, 3 ], [5, 5, 5, 4, 5]]
+students = {"Jonny", "Bilbo", "Steve", "Khendrik","Aaron"}
+students_y = sorted(students)
+
+grade_book [students_y [0]] = (sum( grades [0]) / len( grades [0]))
+grade_book [students_y [1]] = (sum( grades [1]) / len( grades [1]))
+grade_book [students_y [2]] = (sum( grades [2]) / len( grades [2]))
+grade_book [students_y [3]] = (sum( grades [3]) / len( grades [3]))
+grade_book [students_y [4]] = (sum( grades [4]) / len( grades [4]))
+
+print(grade_book)
